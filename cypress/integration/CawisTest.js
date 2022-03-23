bd0c1090547ffe29dc9c9eca42bb75451fe2932d
Ewa Kantorska <ewa.kantorska@tc.gc.ca>
2022-03-22 21:36:04 -0400

    adding new coment

--- cypress/integration/CawisTest.js
+++ cypress/integration/CawisTest.js
@@ -1,4 +1,4 @@
-//https://tcappstest.tc.gc.ca/Saf-Sec-Sur/2/cawis-swimn/ Go to login page//
+//https://tcappstest.tc.gc.ca/Saf-Sec-Sur/2/cawis-swimn/ Go to login page of Cawis//
 
 it ('should be able to open Cawis welcome page', () => {
     cy.visit('https://tcappstest.tc.gc.ca/Saf-Sec-Sur/2/cawis-swimn/')
